e8bd96135adbe83d044d3605ea6ecce36320bb9f
JuneTech <junetech0@gmail.com>
2025-06-04 08:31:06 +0000

    apply uv

---
+++ main.py
@@ -0,0 +1,11 @@
+def main():
+    print("Hello from 2021-0326!\nSee src/commands.sh for available commands.")
+    print(
+        "Example command: "
+        '\nuv run main.py Flexiblejobshop CP 20 0 1 Google 8 "../data/Flexiblejobshop/New" ../Outputs/Flexiblejobshop/New_20250412_Google_8_20s'
+        '\nuv run main.py Hybridflowshop CP 3600 0 1440 Google 8 "../data/Hybridflowshop" ../Outputs/Hybridflowshop/20250412_Google_8_3600s'
+    )
+
+
+if __name__ == "__main__":
+    main()
