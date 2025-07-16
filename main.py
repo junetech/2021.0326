c2754df88f315638c68638ecc53535b03622da7f
JuneTech <junetech0@gmail.com>
2025-07-16 14:52:45 +0900

    update run commands

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 def main():
-    print("Hello from 2021-0326!\nSee src/commands.sh for available commands.")
+    print("Hello from 2021-0326!\Move to `src` directory & see src/commands.sh for available commands.")
     print(
         "Example command: "
         '\nuv run main.py Flexiblejobshop CP 20 0 1 Google 8 "../data/Flexiblejobshop/New" ../Outputs/Flexiblejobshop/New_20250412_Google_8_20s'
-        '\nuv run main.py Hybridflowshop CP 3600 0 1440 Google 8 "../data/Hybridflowshop" ../Outputs/Hybridflowshop/20250412_Google_8_3600s'
+        '\nuv run main.py Hybridflowshop CP 10 0 1 CPLEX 8 ../data/Hybridflowshop ../Outputs/Hybridflowshop/20250716_CPO_8_10s'
     )
 
 
--- src/commands.sh
+++ src/commands.sh
@@ -1,4 +1,6 @@
-python main.py Flexiblejobshop CP 3600 1 20 Google 8 "../data/Flexiblejobshop/Old Benchmarks" ../Outputs/Flexiblejobshop/Old_20250412_Google_8
-python main.py Flexiblejobshop CP 3600 1 20 Google 8 "../data/Flexiblejobshop/New" ../Outputs/Flexiblejobshop/New_20250412_Google_8
-python main.py Flexiblejobshop CP 3600 1 20 CPLEX 8 "../data/Flexiblejobshop/Old Benchmarks" ../Outputs/Flexiblejobshop/Old_20250412_CPO_8
-python main.py Flexiblejobshop CP 3600 1 20 CPLEX 8 "../data/Flexiblejobshop/New" ../Outputs/Flexiblejobshop/New_20250412_CPO_8
+# python main.py Flexiblejobshop CP 3600 1 20 Google 8 "../data/Flexiblejobshop/Old Benchmarks" ../Outputs/Flexiblejobshop/Old_20250412_Google_8
+# python main.py Flexiblejobshop CP 3600 1 20 Google 8 "../data/Flexiblejobshop/New" ../Outputs/Flexiblejobshop/New_20250412_Google_8
+# python main.py Flexiblejobshop CP 3600 1 20 CPLEX 8 "../data/Flexiblejobshop/Old Benchmarks" ../Outputs/Flexiblejobshop/Old_20250412_CPO_8
+# python main.py Flexiblejobshop CP 3600 1 20 CPLEX 8 "../data/Flexiblejobshop/New" ../Outputs/Flexiblejobshop/New_20250412_CPO_8
+# uv run main.py Hybridflowshop CP 10 0 1 CPLEX 8 ../data/Hybridflowshop ../Outputs/Hybridflowshop/20250716_CPO_8_10s
+uv run main.py Hybridflowshop CP 3600 1 1440 CPLEX 8 ../data/Hybridflowshop ../Outputs/Hybridflowshop/20250716_CPO_8_3600s
