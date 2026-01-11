be64b34f727ca564d13ad7895f1f702fe718def4
JuneTech <junetech0@gmail.com>
2026-01-11 13:50:34 +0900

    add more commands

--- src/commands.sh
+++ src/commands.sh
@@ -3,4 +3,7 @@
 # python main.py Flexiblejobshop CP 3600 1 20 CPLEX 8 "../data/Flexiblejobshop/Old Benchmarks" ../Outputs/Flexiblejobshop/Old_20250412_CPO_8
 # python main.py Flexiblejobshop CP 3600 1 20 CPLEX 8 "../data/Flexiblejobshop/New" ../Outputs/Flexiblejobshop/New_20250412_CPO_8
 # uv run main.py Hybridflowshop CP 10 0 1 CPLEX 8 ../data/Hybridflowshop ../Outputs/Hybridflowshop/20250716_CPO_8_10s
-uv run main.py Hybridflowshop CP 3600 1 1440 CPLEX 8 ../data/Hybridflowshop ../Outputs/Hybridflowshop/20250716_CPO_8_3600s
+# uv run main.py Hybridflowshop CP 3600 1 1440 CPLEX 8 ../data/Hybridflowshop ../Outputs/Hybridflowshop/20250716_CPO_8_3600s
+# uv run main.py Tardinessflowshop CP 3600 1 135 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20250919_1q_CPO_8_3600s
+# uv run main.py Tardinessflowshop CP 600 1 135 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20251110_1q_CPO_8_600s
+uv run main.py Hybridflowshop CP 600 1 360 CPLEX 8 ../data/Hybridflowshop ../Outputs/Hybridflowshop/20251112_1q_CPO_8_600s
