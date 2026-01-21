46e6c7a9fdb17366c566f1004abc789a6075fb74
JuneTech <junetech0@gmail.com>
2026-01-21 14:46:24 +0900

    added 20260121 commands

--- src/commands.sh
+++ src/commands.sh
@@ -27,15 +27,27 @@
 # uv run main.py Tardinessflowshop CP 1012.5  76  80 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260114_1_90_CPO_8t
 # uv run main.py Tardinessflowshop CP  337.5  81  85 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260114_1_90_CPO_8t
 # uv run main.py Tardinessflowshop CP 1012.5  86  90 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260114_1_90_CPO_8t
-uv run main.py Tardinessflowshop CP   67.5   1   5 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260115_1_60_CPO_8t
-uv run main.py Tardinessflowshop CP  202.5   6  10 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260115_1_60_CPO_8t
-uv run main.py Tardinessflowshop CP  337.5  11  15 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260115_1_60_CPO_8t
-uv run main.py Tardinessflowshop CP  202.5  16  20 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260115_1_60_CPO_8t
-uv run main.py Tardinessflowshop CP  607.5  21  25 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260115_1_60_CPO_8t
-uv run main.py Tardinessflowshop CP 1012.5  26  30 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260115_1_60_CPO_8t
-uv run main.py Tardinessflowshop CP  337.5  31  35 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260115_1_60_CPO_8t
-uv run main.py Tardinessflowshop CP 1012.5  36  40 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260115_1_60_CPO_8t
-uv run main.py Tardinessflowshop CP 1687.5  41  45 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260115_1_60_CPO_8t
-uv run main.py Tardinessflowshop CP  472.5  46  50 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260115_1_60_CPO_8t
-uv run main.py Tardinessflowshop CP 1417.5  51  55 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260115_1_60_CPO_8t
-uv run main.py Tardinessflowshop CP 2362.5  56  60 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260115_1_60_CPO_8t
+# uv run main.py Tardinessflowshop CP   67.5   1   5 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260115_1_60_CPO_8t
+# uv run main.py Tardinessflowshop CP  202.5   6  10 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260115_1_60_CPO_8t
+# uv run main.py Tardinessflowshop CP  337.5  11  15 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260115_1_60_CPO_8t
+# uv run main.py Tardinessflowshop CP  202.5  16  20 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260115_1_60_CPO_8t
+# uv run main.py Tardinessflowshop CP  607.5  21  25 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260115_1_60_CPO_8t
+# uv run main.py Tardinessflowshop CP 1012.5  26  30 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260115_1_60_CPO_8t
+# uv run main.py Tardinessflowshop CP  337.5  31  35 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260115_1_60_CPO_8t
+# uv run main.py Tardinessflowshop CP 1012.5  36  40 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260115_1_60_CPO_8t
+# uv run main.py Tardinessflowshop CP 1687.5  41  45 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260115_1_60_CPO_8t
+# uv run main.py Tardinessflowshop CP  472.5  46  50 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260115_1_60_CPO_8t
+# uv run main.py Tardinessflowshop CP 1417.5  51  55 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260115_1_60_CPO_8t
+# uv run main.py Tardinessflowshop CP 2362.5  56  60 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260115_1_60_CPO_8t
+uv run main.py Tardinessflowshop CP  112.5   1   5 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260121_1_60_CPO_8t
+uv run main.py Tardinessflowshop CP  337.5   6  10 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260121_1_60_CPO_8t
+uv run main.py Tardinessflowshop CP  562.5  11  15 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260121_1_60_CPO_8t
+uv run main.py Tardinessflowshop CP  337.5  16  20 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260121_1_60_CPO_8t
+uv run main.py Tardinessflowshop CP 1012.5  21  25 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260121_1_60_CPO_8t
+uv run main.py Tardinessflowshop CP 1687.5  26  30 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260121_1_60_CPO_8t
+uv run main.py Tardinessflowshop CP  562.5  31  35 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260121_1_60_CPO_8t
+uv run main.py Tardinessflowshop CP 1687.5  36  40 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260121_1_60_CPO_8t
+uv run main.py Tardinessflowshop CP 2812.5  41  45 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260121_1_60_CPO_8t
+uv run main.py Tardinessflowshop CP  787.5  46  50 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260121_1_60_CPO_8t
+uv run main.py Tardinessflowshop CP 2362.5  51  55 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260121_1_60_CPO_8t
+uv run main.py Tardinessflowshop CP 3937.5  56  60 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260121_1_60_CPO_8t
