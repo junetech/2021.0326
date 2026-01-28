01203525129af0886747930d1b78ac496911b633
JuneTech <junetech0@gmail.com>
2026-01-29 00:39:18 +0900

    added 20260129 commands

--- src/commands.sh
+++ src/commands.sh
@@ -51,5 +51,27 @@
 # uv run main.py Tardinessflowshop CP  787.5  46  50 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260121_1_60_CPO_8t
 # uv run main.py Tardinessflowshop CP 2362.5  51  55 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260121_1_60_CPO_8t
 # uv run main.py Tardinessflowshop CP 3937.5  56  60 CPLEX 8 ../data/Tardinessflowshop ../Outputs/Tardinessflowshop/20260121_1_60_CPO_8t
-uv run main.py Hybridflowshop CP  4   1   1 CPLEX 8 ../data/ff2020big ../Outputs/ff2020big/20260129_1_24_CPO_8t
-uv run main.py Hybridflowshop CP  8   2   2 CPLEX 8 ../data/ff2020big ../Outputs/ff2020big/20260129_1_24_CPO_8t
+uv run main.py Hybridflowshop CP  400   1   1 CPLEX 8 ../data/ff2020big ../Outputs/ff2020big/20260129_1_24_CPO_8t
+uv run main.py Hybridflowshop CP  800   2   2 CPLEX 8 ../data/ff2020big ../Outputs/ff2020big/20260129_1_24_CPO_8t
+uv run main.py Hybridflowshop CP 1200   3   3 CPLEX 8 ../data/ff2020big ../Outputs/ff2020big/20260129_1_24_CPO_8t
+uv run main.py Hybridflowshop CP 1600   4   4 CPLEX 8 ../data/ff2020big ../Outputs/ff2020big/20260129_1_24_CPO_8t
+uv run main.py Hybridflowshop CP  800   5   5 CPLEX 8 ../data/ff2020big ../Outputs/ff2020big/20260129_1_24_CPO_8t
+uv run main.py Hybridflowshop CP 1600   6   6 CPLEX 8 ../data/ff2020big ../Outputs/ff2020big/20260129_1_24_CPO_8t
+uv run main.py Hybridflowshop CP 2400   7   7 CPLEX 8 ../data/ff2020big ../Outputs/ff2020big/20260129_1_24_CPO_8t
+uv run main.py Hybridflowshop CP 3200   8   8 CPLEX 8 ../data/ff2020big ../Outputs/ff2020big/20260129_1_24_CPO_8t
+uv run main.py Hybridflowshop CP 1200   9   9 CPLEX 8 ../data/ff2020big ../Outputs/ff2020big/20260129_1_24_CPO_8t
+uv run main.py Hybridflowshop CP 2400  10  10 CPLEX 8 ../data/ff2020big ../Outputs/ff2020big/20260129_1_24_CPO_8t
+uv run main.py Hybridflowshop CP 3600  11  11 CPLEX 8 ../data/ff2020big ../Outputs/ff2020big/20260129_1_24_CPO_8t
+uv run main.py Hybridflowshop CP 4800  12  12 CPLEX 8 ../data/ff2020big ../Outputs/ff2020big/20260129_1_24_CPO_8t
+uv run main.py Hybridflowshop CP 1600  13  13 CPLEX 8 ../data/ff2020big ../Outputs/ff2020big/20260129_1_24_CPO_8t
+uv run main.py Hybridflowshop CP 3200  14  14 CPLEX 8 ../data/ff2020big ../Outputs/ff2020big/20260129_1_24_CPO_8t
+uv run main.py Hybridflowshop CP 4800  15  15 CPLEX 8 ../data/ff2020big ../Outputs/ff2020big/20260129_1_24_CPO_8t
+uv run main.py Hybridflowshop CP 6400  16  16 CPLEX 8 ../data/ff2020big ../Outputs/ff2020big/20260129_1_24_CPO_8t
+uv run main.py Hybridflowshop CP 2000  17  17 CPLEX 8 ../data/ff2020big ../Outputs/ff2020big/20260129_1_24_CPO_8t
+uv run main.py Hybridflowshop CP 4000  18  18 CPLEX 8 ../data/ff2020big ../Outputs/ff2020big/20260129_1_24_CPO_8t
+uv run main.py Hybridflowshop CP 6000  19  19 CPLEX 8 ../data/ff2020big ../Outputs/ff2020big/20260129_1_24_CPO_8t
+uv run main.py Hybridflowshop CP 8000  20  20 CPLEX 8 ../data/ff2020big ../Outputs/ff2020big/20260129_1_24_CPO_8t
+uv run main.py Hybridflowshop CP 2400  21  21 CPLEX 8 ../data/ff2020big ../Outputs/ff2020big/20260129_1_24_CPO_8t
+uv run main.py Hybridflowshop CP 4800  22  22 CPLEX 8 ../data/ff2020big ../Outputs/ff2020big/20260129_1_24_CPO_8t
+uv run main.py Hybridflowshop CP 7200  23  23 CPLEX 8 ../data/ff2020big ../Outputs/ff2020big/20260129_1_24_CPO_8t
+uv run main.py Hybridflowshop CP 9600  24  24 CPLEX 8 ../data/ff2020big ../Outputs/ff2020big/20260129_1_24_CPO_8t
